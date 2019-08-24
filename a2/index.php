d23132730493bf7afc1d2b11cd89b8fced0062e7
s3784464 <s3784464@student.rmit.edu.au>
2019-08-24 23:13:54 +1000

    A2 - Most of HTML parts done

--- a2/index.php
+++ a2/index.php
@@ -14,24 +14,111 @@
   <body>
 
     <header>
-      <div>Put company logo and name here</div>
+      <div>Lunardo Cinema <img src =../../media/logo.png/> </div>
     </header>
 
     <nav>
-      <div>Put website navigation links here</div>
+      <div>
+          <a href= '#About Us'>About Us</a>
+          <a href= '#Prices'>Prices</a>
+          <a href= '#Now Showing'>Now Showing</a>
+      </div>
     </nav>
 
     <main>
-      <article id='Website Under Construction'>
-    <!-- Creative Commons image sourced from https://pixabay.com/en/maintenance-under-construction-2422173/ and used for educational purposes only -->
-        <img src='../../media/website-under-construction.png' alt='Website Under Construction' />
+      <article id='About Us'>
+        <p>Lunardo Cinema has reopened after extensive improvements and renovations!<p>
+        <p>Our <b>Standard</b> cinema seats have been all received an upgrade <img width=10% height=10% src='../../media/standard.jpg'> <br>
+        and we have also finally opened our new <b>First Class</b> cinema! <img width=10% height=10% src='../../media/firstClass.png'></p>
+        <p>We're also happy to announce that our projection and sound systems are upgraded with <a href='https://www.dolby.com/us/en/brands/dolby-vision.html'>3D Dolby Vision </a> 
+        and <a href='https://www.dolby.com/us/en/technologies/cinema/dolby-atmos.html'> Dolby Atmos</a> sound.<p>
+      </article>
+
+      <article id ='Prices'>
+        <table border= 1>
+            <tr>
+              <td>Seat Type</td>
+              <td>Seat Code</td>
+              <td>All day Monday and Wednesday AND 12pm on Weekdays</td>
+              <td>All other times</td>
+            </tr>
+            <tr>
+              <td>Standard Adult</td>
+              <td>STA</td>
+              <td>14.00</td>
+              <td>19.80</td>
+            </tr>
+            <tr>
+              <td>Standard Concession</td>
+              <td>STP</td>
+              <td>12.50</td>
+              <td>17.50</td>
+            </tr>
+            <tr>
+              <td>Standard Child</td>
+              <td>STC</td>
+              <td>11.00</td>
+              <td>15.30</td>
+            </tr>
+            <tr>
+              <td>First Class Adult</td>
+              <td>FCA</td>
+              <td>24.00</td>
+              <td>30.00</td>
+            </tr>
+            <tr>
+              <td>First Class Concession</td>
+              <td>FCP</td>
+              <td>22.50</td>
+              <td>27.00</td>
+            </tr>
+            <tr>
+              <td>First Class Child</td>
+              <td>FCC</td>
+              <td>21.00</td>
+              <td>24.00</td>
+            </tr>
+        </table>
+      </article>
+
+      <article id ='Now Showing'>
+        <div>
+          Avengers: Endgame
+        </div>
+        <div>
+          Top End Wedding
+        </div>
+        <div>
+          Dumbo
+        </div>
+        <div>
+          The Happy Prince
+        </div>
+
+        <h1>Avengers: Endgame</h1>
+        <h3>Synopsis</h3>
+        <p>After the devastating events of <b>Avengers: Infinity War (2018)</b>, the universe is in ruins. With the help of remaining allies, 
+        the Avengers assemble once more in order to reverse Thanos' actions and restore balance to the universe. (IMDB Avengers: Endgame 2019)</p>
+        <iframe width ="350" height = "200" src="https://www.youtube.com/embed/TcMBFSGVi1c" frameborder=0 allow="accelerometer; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+        <h3>Make a Booking:</h3>
+        <!--Provide session times-->
+        <p>WIP: <i>session times</i></p>
+
+        <!--Create a 'booking form' option-->
+        <p>WIP: <i>booking form</i></p>
       </article>
     </main>
 
     <footer>
-      <div>&copy;<script>
+      <div>&copy;
+      <script>
         document.write(new Date().getFullYear());
-      </script> Put your name(s), student number(s) and group name here. Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
+      </script>
+      lunardocinema@gmail.com - 0412 345 678 - 
+      <a href='https://www.google.com/maps/place/123+La+Trobe+St,+Melbourne+VIC+3000/@-37.8086568,144.9655973,17z/data=!3m1!4b1!4m5!3m4!1s0x6ad642cebf245a07:0x258769ae7754af!8m2!3d-37.8086568!4d144.967786'>
+      123 La Trobe St, Melbourne</a><br>
+      Jonathon Mitterbacher - s3784464 <a href='https://github.com/s3784464/wp'>- GitHub Repository </a>- Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
       <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
       <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
     </footer>
