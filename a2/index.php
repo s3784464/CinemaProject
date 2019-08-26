1c0eac6f6657fdbd5b92596787a38cce6549e6b5
s3784464 <s3784464@student.rmit.edu.au>
2019-08-26 12:35:37 +1000

    css beginnings

--- a2/index.php
+++ a2/index.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Assignment 2</title>
-    
+
     <!-- Keep wireframe.css for debugging, add your css to style.css -->
     <link id='wireframecss' type="text/css" rel="stylesheet" href="../wireframe.css" disabled>
     <link id='stylecss' type="text/css" rel="stylesheet" href="css/style.css">
@@ -12,9 +12,10 @@
   </head>
 
   <body>
-
     <header>
-      <div>Lunardo Cinema <img src =../../media/logo.png/> </div>
+      <div id="mainHeading">
+        <h1>Lunardo Cinema</h1> <img src =../../media/logo.png/> 
+      </div>
     </header>
 
     <nav>
@@ -117,7 +118,8 @@
       </script>
       lunardocinema@gmail.com - 0412 345 678 - 
       <a href='https://www.google.com/maps/place/123+La+Trobe+St,+Melbourne+VIC+3000/@-37.8086568,144.9655973,17z/data=!3m1!4b1!4m5!3m4!1s0x6ad642cebf245a07:0x258769ae7754af!8m2!3d-37.8086568!4d144.967786'>
-      123 La Trobe St, Melbourne</a><br>
+        123 La Trobe St, Melbourne
+      </a><br>
       Jonathon Mitterbacher - s3784464 <a href='https://github.com/s3784464/wp'>- GitHub Repository </a>- Last modified <?= date ("Y F d  H:i", filemtime($_SERVER['SCRIPT_FILENAME'])); ?>.</div>
       <div>Disclaimer: This website is not a real website and is being developed as part of a School of Science Web Programming course at RMIT University in Melbourne, Australia.</div>
       <div><button id='toggleWireframeCSS' onclick='toggleWireframe()'>Toggle Wireframe CSS</button></div>
